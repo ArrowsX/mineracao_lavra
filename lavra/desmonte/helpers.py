051d5e02c22a95584b1a4dd23083caf07bedfdec
Tafarel Yan <tafarel.yan@gmail.com>
2016-11-14 19:15:09 -0200

    Update

--- lavra/desmonte/helpers.py
+++ lavra/desmonte/helpers.py
@@ -2,9 +2,11 @@ from io import BytesIO
 from math import fabs, cos, pi
 
 from django.http import HttpResponse
-from reportlab.pdfgen import canvas 
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle, TA_CENTER
+from reportlab.lib.units import inch, mm
+from reportlab.pdfgen import canvas 
+from reportlab.platypus import Paragraph, SimpleDocTemplate, Table, Spacer
 
 
 def process(data):
@@ -36,6 +38,88 @@ def process(data):
     n = vfogo / vr
     r = vr / l
     ce = qt / vr
+
+
+class Test(object):
+
+    def __init__(self):
+        self.width, self.height = letter
+        self.styles = getSampleStyleSheet()
+
+    def coord(self, x, y, unit=1):
+        x, y = x * unit, self.height - y * unit
+        return x, y
+    
+    def run(self):
+        self.doc = SimpleDocTemplate('test.pdf')
+        self.story = [Spacer(1, 2.5*inch)]
+        self.createLineItems()
+
+        self.doc.build(self.story, onFirstPage=self.createDocument)
+        print("Finished")
+
+    def createDocument(self, canvas, doc):
+
+        self.c = canvas
+        normal = self.styles["Normal"]
+
+        header_text = "<b>This is a test header</b>"
+        p = Paragraph(header_text, normal)
+        p.wrapOn(self.c, self.width, self.height)
+        p.drawOn(self.c, *self.coord(100, 12, mm))
+
+        ptext = """Lorem ipsum dolor sit amet, consectetur adipisicing elit"""
+
+        p = Paragraph(ptext, style=normal)
+        p.wrapOn(self.c, self.width-50, self.height)
+        p.drawOn(self.c, 30, 700)
+
+        ptext = """Lorem ipsum dolor sit amet, consectetur adipisicing elit"""
+
+        p = Paragraph(ptext, style=normal)
+        p.wrapOn(self.c, self.width-50, self.height)
+        p.drawOn(self.c, 30, 600)
+
+        ptext = """Lorem ipsum dolor sit amet, consectetur adipisicing elit"""
+
+    def createLineItems(self):
+
+        text_data = ["Line", "DOS", "Procedure<br/>/Modifier",
+                     "Description", "Units", "Billed<br/>Charges",
+                     "Type1<br/>Reductions", "Type2<br/>Reductions",
+                     "Type3<br/>Reductions", "Allowance",
+                     "Qualify<br/>Code"]
+        d = []
+        font_size = 8
+        centered = ParagraphStyle(name="centered", aligment=TA_CENTER)
+
+        for text in text_data:
+            ptext = "<font size=%s><b>%s</b></font>" % (font_size, text)
+            p = Paragraph(ptext, centered)
+            d.append(p)
+
+        data = [d]
+        
+        line_num = 1
+
+        formatted_line_data = []
+
+        for x in range(200):
+            line_data = [str(line_num), "04/12/2013", "73090", 
+                                          "Test Reflexes", "1", "131.00", "0.00", 
+                                          "0.00", "0.00", "0.00", "1234"]
+
+
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+
+        p = canvas.Canvas(response)
+
+        p.drawString(100, 100, "Hello world.")
+        
+        p.showPage()
+        p.save()
+        return response
 
 def select_diameter(rcu, pmh):
     ds = 65, 89, 150
